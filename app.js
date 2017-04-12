e09a0f891e9d226f9533914f864d66d248720b82
ckcks12 <wyoducc@gmail.com>
2017-04-12 18:19:59 +0900

    *add express proxy and subdomain for api gateway implement

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var proxy = require('proxy-express');
+var subdomain = require('express-subdomain');
 
 var app = express();
 
@@ -20,27 +19,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+var webIp = '10.99.0.15';
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(subdomain('lantern', proxy(webIp + '/lantern', '/')));
+app.use(proxy(webIp + '/thresh', '/'));
 
 module.exports = app;
